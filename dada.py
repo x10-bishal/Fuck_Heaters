57c57154f8459152e70285ec9f2362fe5126d4f9
Bishal The Game Changer <147220367+x10-bishal@users.noreply.github.com>
2023-10-09 19:58:17 +0600

    Create dada.py

---
+++ dada.py
@@ -0,0 +1,72 @@
+<h1 align="center">X10 BOMBER 3.0
+<img src="logo.jpg"><br>
+</h1>
+
+* `💣 📱 💀`<br />
+* `A Superfast SMS & Call bomber for Linux And Termux !`
+
+## Disclaimer
+*This tool is for educational purposes only !*
+_Don't use this to take revenge_<br />
+*I will not be responsible for any misuse*
+
+## About
+* `Unlimited Usage !`
+* `Cross Platform`
+* `Supports newest Android also`
+* `No balance will be deducted to send SMS/calls`
+* `Working Apis`
+* `No missing SMS issues, all messages will be sent.`
+* `Working with all Operators/Carriers`
+
+## Tested On :
+<ul>
+  <li>Kali Linux</li>
+  <li>Termux</li>
+  <li>Ubuntu</li>
+  <li>Parrot Sec OS</li>
+  <li>Kali nethunter</li>
+  <li>Alpine linux</li>
+  
+</ul>
+
+## Termux Issue:
+* `Termux App is no longer recieving updates on playstore`
+* `due to recently introduced Google Play policy `
+<br>
+
+DON'T WORRY
+* `We have a solution for that !`
+<br>
+
+
+You can download the latest termux app and install it
+
+From here <a href="https://f-droid.org/repo/com.termux_117.apk">Link</a>
+
+## Usage
+
+
+
+#### For Termux
+
+Update the packages
+```bash
+pkg up -y
+```
+Install some dependencies
+```bash
+pkg install git wget python3 -y
+```
+Clone the repository
+```bash
+git clone https://github.com/x10-Bishal/X10_BOMBER
+```
+Go to the X10 directory
+```bash
+cd X10_BOMBER
+```
+Run the script
+```bash
+python X10.py
+```
